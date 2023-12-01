2956446306dd716c4415a6c85eef9cd30bff9c91
DAVLIMA <davimagi1234@gmail.com>
2023-12-01 18:08:54 -0300

    inicial

--- App.js
+++ App.js
@@ -1,12 +1,117 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { 
+  StyleSheet, 
+  Text, 
+  View, 
+  TextInput, 
+  TouchableOpacity, 
+  FlatList, 
+  KeyboardAvoidingView, 
+  Platform, 
+  Keyboard, 
+  Alert,
+} from "react-native";
+import { Entypo, MaterialIcons } from '@expo/vector-icons';
+import task from './banco/task';
+
+import { LogBox } from 'react-native';
+
+LogBox.ignoreLogs(['new NativeEventEmitter']); // Ignore log notification by message
+LogBox.ignoreAllLogs(); //Ignore all log notifications
 
 export default function App() {
+
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState('');
+
+
+  async function addTask() {
+    
+    if (newTask == ' ') {
+      return;
+    }
+
+    const search = task.filter(task => task == newTask);
+
+    if (search.length != 0) {
+      Alert.alert("Atenção", "Nome da tarefa repitido!");
+      return;
+    }
+
+    setTask([...task, newTask]);
+    setNewTask(" ");
+
+    Keyboard.dismiss();
+  } 
+  
+  async function removeTask(item) {
+    Alert.alert(
+      "Deletar Task",
+      "Tem certeza que deseja remover esta anotação",
+      [
+        {
+          text: "Cancel",
+          onPress: () => {
+            return;
+          },
+          style: 'cancel'
+        },
+        {
+          text: "OK",
+          onPress: () => setTask(task.filter(task => task != item))
+        }
+      ],
+      { cancelable: false}
+    );
+  }
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+    <KeyboardAvoidingView
+      keyboardVerticalOffset={0}
+      behavior="padding"
+      style={{ flex: 1 }}
+      enabled={ Platform.OS == 'ios' }
+    >
+      <View style={styles.container}>
+        <View style={styles.Body}>
+          <FlatList 
+            style={styles.FlatList} 
+            data={task}
+            keyExtractor={item => item.toString()}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <View style={styles.ContainerView}>
+                <Text style={styles.Texto}>{item}</Text>
+                <TouchableOpacity onPress={() => removeTask(item)}>
+                  <MaterialIcons 
+                    name="delete-forever" 
+                    size={25} 
+                    color="#f64c75"
+                  />
+                </TouchableOpacity>
+              </View>
+          )}
+          />
+        </View>
+        <View style={styles.Form}>
+          <TextInput 
+          style={styles.Input}
+          placeholderTextColor='#999'
+          autoCorrect={true}
+          placeholder="Adicione uma tarefa " 
+          maxLength={25}
+          onChangeText={text => setNewTask(text)}
+          value={newTask}
+          />
+          <TouchableOpacity style={styles.Button} onPress={() => addTask()}>
+            <Entypo name="add-to-list" size={24} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      </KeyboardAvoidingView>
+    </>
   );
 }
 
@@ -14,7 +119,64 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 20,
+    paddingVertical: 20,
+    marginTop: 20
   },
+  Body: {
+    flex: 1
+  },
+  Form: {
+    padding: 0,
+    height: 60,
+    justifyContent: 'center',
+    alignSelf: 'stretch',
+    flexDirection: 'row',
+    paddingTop: 13,
+    borderTopWidth: 1,
+    borderColor: '#eee'
+  },
+  Input: {
+    flex: 1,
+    height: 40,
+    backgroundColor: '#eee',
+    borderRadius: 4,
+    paddingVertical: 10,
+    paddingHorizontal: 10,
+    borderWidth: 10,
+    borderColor: '#eee'
+  },
+  Button: {
+    height: 40,
+    width: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#1c6cce',
+    borderRadius: 4,
+    marginLeft: 10
+  },
+  FlatList: {
+    flex: 1,
+    marginTop: 5
+  },
+  ContainerView: {
+    marginBottom: 15,
+    padding: 15,
+    borderRadius: 4,
+    backgroundColor: '#eee',
+
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    borderWidth: 1,
+    borderColor: '#eee'
+  },
+  Texto: {
+    fontSize: 14,
+    color: '#333',
+    fontWeight: 'bold',
+    marginTop: 4,
+    textAlign: 'center'
+  }
 });
---
+++ banco/task.js
@@ -0,0 +1,137 @@
+import db from "./DatabaseConnection";
+
+
+/**
+ * INICIALIZAÇÃO DA TABELA
+ * - Executa sempre, mas só cria a tabela caso não exista (primeira execução)
+ */
+db.transaction((tx) => {
+  tx.executeSql(
+    "CREATE TABLE IF NOT EXISTS Tarefas (id INTEGER PRIMARY KEY AUTOINCREMENT, descricao TEXT);"
+  );  
+});
+
+/**
+ * CRIAÇÃO DE UM NOVO REGISTRO
+ * - Recebe um objeto;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é o ID do registro (criado por AUTOINCREMENT)
+ *  - Pode retornar erro (reject) caso exista erro no SQL ou nos parâmetros.
+ */
+const create = (obj) => {
+  if (typeof obj.descricao !== "string") {
+    throw new Error("O valor da propriedade 'descricao' deve ser do tipo 'string'.");
+  }
+
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "INSERT INTO Tarefas (descricao) values (?);",
+        [obj.descricao],
+        //-----------------------
+        (_, { rowsAffected, insertId }) => {
+          if (rowsAffected > 0) resolve(insertId);
+          else reject("Erro ao inserir obj: " + JSON.stringify(obj)); // Falha na inserção
+        },
+        (_, error) => reject(error) 
+      );
+    });
+  });
+};
+
+
+const find = (id) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM Tarefas WHERE id=?;",
+        [id],
+        //-----------------------
+        (_, { rows }) => {
+          if (rows.length > 0) resolve(rows._array[0]);
+          else reject("Obj not found: id=" + id); 
+        },
+        (_, error) => reject(error) 
+      );
+    });
+  });
+};
+
+/**
+ * BUSCA UM REGISTRO POR MEIO DA MARCA (brand)
+ * - Recebe a marca do carro;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é um array com os objetos encontrados;
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL;
+ *  - Pode retornar um array vazio caso nenhum objeto seja encontrado.
+ */
+const findByBrand = (brand) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+
+      tx.executeSql(
+        "SELECT * FROM Tarefas WHERE descricao LIKE ?;",
+        [brand],
+        //-----------------------
+        (_, { rows }) => {
+          if (rows.length > 0) resolve(rows._array);
+          else reject("Obj not found: brand=" + brand); 
+        },
+        (_, error) => reject(error) 
+      );
+    });
+  });
+};
+
+/**
+ * BUSCA TODOS OS REGISTROS DE UMA DETERMINADA TABELA
+ * - Não recebe parâmetros;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise é uma lista (Array) de objetos;
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL;
+ *  - Pode retornar um array vazio caso não existam registros.
+ */
+const all = () => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "SELECT * FROM Tarefas;",
+        [],
+        //-----------------------
+        (_, { rows }) => resolve(rows._array),
+        (_, error) => reject(error) 
+      );
+    });
+  });
+};
+
+/**
+ * REMOVE UM REGISTRO POR MEIO DO ID
+ * - Recebe o ID do registro;
+ * - Retorna uma Promise:
+ *  - O resultado da Promise a quantidade de registros removidos (zero indica que nada foi removido);
+ *  - Pode retornar erro (reject) caso o ID não exista ou então caso ocorra erro no SQL.
+ */
+const remove = (id) => {
+  return new Promise((resolve, reject) => {
+    db.transaction((tx) => {
+      tx.executeSql(
+        "DELETE FROM Tarefas WHERE id=?;",
+        [id],
+        //-----------------------
+        (_, { rowsAffected }) => {
+          resolve(rowsAffected);
+        },
+        (_, error) => reject(error) 
+      );
+    });
+  });
+};
+
+export default {
+  create,
+  find,
+  findByBrand,
+  all,
+  remove,
+};
